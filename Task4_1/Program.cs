d1ffc15a32cdadd882dd306f6b2c643ad276fac6
Larisa Samoilenko <qwry28@gmail.com>
2021-09-03 11:20:20 +0300

    Add project files.

---
+++ Task4_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Task4_Library;
+
+namespace Task4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 20;
+            int[] array = new int[n];
+            Random random = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = random.Next(0, 101);
+                Console.Write($"{array[i]} ");
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine(TaskHelper.Task4_1(array));
+        }
+    }
+}
---
+++ Task4_Library/TaskHelper.cs
@@ -0,0 +1,20 @@
+﻿namespace Task4_Library
+{
+    public class TaskHelper
+    {
+        public static int Task4_1(int[] array) 
+        {
+            int min = array[0];
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (min > array[i])
+                {
+                    min = array[i];
+                }
+            }
+
+            return min;
+        }
+    }
+}
